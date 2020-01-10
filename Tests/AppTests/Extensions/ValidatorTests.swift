07307bb7e85e1b46cb2a97221a5bc50cb07deac7
Vyacheslav <vorona.vyacheslav@gmail.com>
2020-01-11 00:19:48 +0900

    ValidatorTests

---
+++ Tests/AppTests/Extensions/ValidatorTests.swift
@@ -0,0 +1,23 @@
+//
+//  ValidatorTests.swift
+//  AppTests
+//
+//  Created by Vorona Vyacheslav on 1/11/20.
+//
+
+import App
+import XCTest
+import Validation
+
+final class ValidatorTests: XCTestCase {
+
+    func testContains() throws {
+        try Validator<String>.contains(.alphanumerics).validate("A#$%")
+        try Validator<String>.contains(.alphanumerics).validate("1,-?")
+        try Validator<String>.contains(.letters).validate("Z1))")
+        try Validator<String>.contains(.uppercaseLetters).validate("1*bF")
+        XCTAssertThrowsError(try Validator<String>.contains(.alphanumerics).validate("#$)*"))
+        XCTAssertThrowsError(try Validator<String>.contains(.letters).validate("#$)*09"))
+        XCTAssertThrowsError(try Validator<String>.contains(.uppercaseLetters).validate("yuo"))
+    }
+}
